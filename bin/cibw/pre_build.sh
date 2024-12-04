e832c9f8f902291c5ca49261a0ab53fa555172ac
TheGupta2012 <harshit.11235@gmail.com>
2024-12-04 11:48:32 +0530

    add checks in script

--- bin/cibw/pre_build.sh
+++ bin/cibw/pre_build.sh
@@ -31,11 +31,27 @@ pip install setuptools wheel cython
 
 # Test if we are running the build for pre-release version 
 if [[ ${PRE_RELEASE_BUILD:-false} == "true" ]]; then
-    echo "Running pre-release pre-build"
+    echo "Running pre-release changes"
 
     # Install required packages
     python -m pip install -U toml-cli qbraid-core
-    export PRE_RELEASE_VERSION=$(python $project/bin/stamp_pre_release.py pyqasm)
+    deps_exit_code=$?
+
+    if [ $deps_exit_code -ne 0 ]; then
+        echo "Failed to install dependencies for pre-release changes"
+        exit 1
+    fi
+
+    # Set the PRE_RELEASE_VERSION variable
+    PRE_RELEASE_VERSION=$(python $project/bin/stamp_pre_release.py pyqasm)
+    version_exit_code=$?
+
+    if [ $version_exit_code -ne 0 ]; then
+        echo "Failed to set PRE_RELEASE_VERSION"
+        exit 1
+    fi
+
+    export PRE_RELEASE_VERSION
 
     # exit if the version is not pre-release
     [[ "$PRE_RELEASE_VERSION" =~ .*(-a\.|-b\.|-rc\.).* ]] && echo "Deploying pre-release version '$PRE_RELEASE_VERSION'" || (echo "not pre-release version"; exit 0)
