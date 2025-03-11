a930da1bcc4984e613a2e062219124d3a1006ea9
PranavTupe2000 <iamgroot5820@gmail.com>
2025-03-11 16:59:46 +0530

    Added module docstring for algorithm module

--- src/pyqasm/algorithms/__init__.py
+++ src/pyqasm/algorithms/__init__.py
@@ -1,3 +1,12 @@
+"""
+Sub module for quantum algorithms.
+
+Functions:
+----------
+    solovay_kitaev: Solovay-Kitaev algorithm for approximating unitary gates.
+
+"""
+
 from pyqasm.algorithms.solovay_kitaev.solovay_kitaev import solovay_kitaev
 
 __all__ = [
