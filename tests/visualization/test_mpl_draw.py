aa0e00f056c04c3030383603633adf1195a63ce1
Harshit Gupta <harshit.11235@gmail.com>
2025-04-08 11:10:42 +0530

    Fix draw bug in pyqasm printer  (#168)
    * add close and interactive check for draw
    
    * fix test
    
    * bump cov

--- tests/visualization/test_mpl_draw.py
+++ tests/visualization/test_mpl_draw.py
@@ -16,11 +16,13 @@
 Tests for the QASM printer module.
 """
 
+import os
+import unittest.mock
+
 import pytest
 
-from pyqasm import draw
 from pyqasm.entrypoint import loads
-from pyqasm.printer import mpl_draw
+from pyqasm.printer import draw, mpl_draw
 
 pytest.importorskip("matplotlib", reason="Matplotlib not installed.")
 
@@ -111,7 +113,7 @@ def test_draw_bell():
     cnot q[0], q[1];
     b = measure q;
     """
-    fig = draw(qasm3, output="mpl")
+    fig = mpl_draw(qasm3)
     return fig
 
 
@@ -134,5 +136,72 @@ def test_draw_misc_ops():
     measure q;
     reset q;
     """
-    fig = draw(qasm3, output="mpl")
+    fig = mpl_draw(qasm3)
     return fig
+
+
+def test_draw_raises_unsupported_format_error():
+    """Test that an error is raised for unsupported formats."""
+    qasm = """
+    OPENQASM 3.0;
+    include "stdgates.inc";
+    qubit[2] q;
+    h q;
+    cnot q[0], q[1];
+    measure q;
+    """
+    circ = loads(qasm)
+
+    with pytest.raises(ValueError, match=r"Unsupported output format"):
+        draw(circ, output="unsupported_format")
+
+
+def test_saved_figure():
+    """Test that the saved figure is not None."""
+    qasm = """
+    OPENQASM 3.0;
+    include "stdgates.inc";
+    qubit[2] q;
+    h q;
+    cnot q[0], q[1];
+    measure q;
+    """
+
+    circ = loads(qasm)
+    fig = mpl_draw(circ, filename="test_img.png")
+    assert fig is not None
+
+    # check the file exists
+    assert os.path.exists("test_img.png")
+
+    # clean up the file
+    os.remove("test_img.png")
+
+
+@pytest.mark.parametrize("is_interactive", [True, False])
+def test_matplotlib_interactive_behavior(is_interactive):
+    """Test plt.show() behavior depending on matplotlib's interactive mode."""
+    qasm = """
+    OPENQASM 3.0;
+    include "stdgates.inc";
+    qubit[2] q;
+    h q;
+    cnot q[0], q[1];
+    measure q;
+    """
+
+    with unittest.mock.patch(
+        "matplotlib.pyplot.isinteractive", return_value=is_interactive
+    ) as mock_isinteractive:
+        with unittest.mock.patch("matplotlib.pyplot.show") as mock_show:
+            # Call draw function
+            draw(qasm, output="mpl")
+
+            # Verify isinteractive was called
+            mock_isinteractive.assert_called()
+
+            # When not interactive, show should be called; otherwise it shouldn't
+            if is_interactive:
+                mock_show.assert_not_called()
+            else:
+                mock_show.assert_called_once()
