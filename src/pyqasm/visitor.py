dd1317e11c48c276bde88f632868a4c7c59c61be
TheGupta2012 <harshit.11235@gmail.com>
2025-05-01 13:33:15 +0530

    use copy instead of deepcopy

--- src/pyqasm/visitor.py
+++ src/pyqasm/visitor.py
@@ -1098,6 +1098,7 @@ class QasmVisitor:
         Returns:
             None
         """
+        operation, ctrls = copy.copy(operation), copy.copy(ctrls)
         negctrls = []
         if ctrls is None:
             ctrls = []
