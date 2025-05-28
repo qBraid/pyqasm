b56be8425a5b7c7ee623aa6046175f6fdbfb18ce
arunjmoorthy <arun.j.moorthy@gmail.com>
2025-05-28 01:09:53 -0700

    test_import updated

--- tests/pulse/test_import.py
+++ tests/pulse/test_import.py
@@ -1,8 +1,5 @@
 import pytest
 
 def test_openpulse_import():
-    """Tests that openpulse can be imported."""
-    try:
-        import openpulse
-    except ImportError:
-        pytest.fail("Failed to import openpulse. Ensure that pyqasm[pulse] is installed.")
+    """Tests that openpulse can be imported if pyqasm[pulse] is installed."""
+    pytest.importorskip("openpulse")
