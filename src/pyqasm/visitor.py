d5dae9cfbaaf55f8e22e4bb6ff83e95468ccf123
Bhagyasree Yadlapalli <133559120+bhagyasreey@users.noreply.github.com>
2025-06-12 10:28:19 +0530

    Update visitor.py

--- src/pyqasm/visitor.py
+++ src/pyqasm/visitor.py
@@ -2280,7 +2280,7 @@ class QasmVisitor:
             qasm3_ast.SubroutineDefinition: self._visit_subroutine_definition,
             qasm3_ast.ExpressionStatement: lambda x: self._visit_function_call(x.expression),
             qasm3_ast.IODeclaration: lambda x: [],
-            qasm3_ast.BreakStatement: x: self._visit_break(x),
+            qasm3_ast.BreakStatement: lambda x: self._visit_break(x),
             qasm3_ast.ContinueStatement: lambda x: self._visit_continue(x),
         }
 
